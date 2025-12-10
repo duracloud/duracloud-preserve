86e694b64e40f96db6c22ceef8869d5f0b202271
Mark Cooper <551470+mark-cooper@users.noreply.github.com>
2025-12-09 21:14:08 -0800

    Addtl. prep for err handling

--- bucket-request/src/app.rs
+++ bucket-request/src/app.rs
@@ -1,6 +1,5 @@
 use awsutils::{bucket::RequestConfig, file::File};
-use lambda_runtime::tracing;
-use lambda_runtime::Error;
+use lambda_runtime::{tracing, Error};
 
 pub(crate) async fn perform(config: &RequestConfig, file: &File) -> Result<(), Error> {
     tracing::info!("Retrieving request file from S3");
@@ -10,7 +9,7 @@ pub(crate) async fn perform(config: &RequestConfig, file: &File) -> Result<(), E
         Err(e) => {
             tracing::error!("Error getting bucket names: {}", e);
             // TODO: upload error report
-            return Ok(());
+            return Ok(()); // User or transitory error. Try again.
         }
     };
 
@@ -22,7 +21,7 @@ pub(crate) async fn perform(config: &RequestConfig, file: &File) -> Result<(), E
         Err(e) => {
             tracing::error!("Error parsing bucket names: {}", e);
             // TODO: upload error report
-            return Ok(());
+            return Ok(()); // User error. Try again.
         }
     };
 
@@ -32,7 +31,9 @@ pub(crate) async fn perform(config: &RequestConfig, file: &File) -> Result<(), E
     let issues = awsutils::bucket::create_buckets(config, &buckets).await;
     if issues.len() > 0 {
         // TODO: upload the issues
-        eprintln!("{:?}", issues);
+        tracing::error!("{:?}", issues);
+        // Unexpected error, propagate for alert
+        return Err(Error::from("Failed to create one or more buckets"));
     }
 
     tracing::info!("Perform complete");

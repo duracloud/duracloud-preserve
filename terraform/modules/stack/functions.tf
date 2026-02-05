83b14642e5d3366bf3bde43e4862e8d2cc2d7673
Mark Cooper <551470+mark-cooper@users.noreply.github.com>
2026-02-04 17:12:48 -0800

    Prep tf functions

--- terraform/modules/stack/functions.tf
+++ terraform/modules/stack/functions.tf
@@ -1,1 +1,11 @@
-# TODO
+locals {
+  deploy_functions = var.deploy_functions
+
+  functions = {
+    bucket-request    = {}
+    checksum-report   = {}
+    compute-checksums = {}
+    inventory-report  = {}
+    storage-report    = {}
+  }
+}

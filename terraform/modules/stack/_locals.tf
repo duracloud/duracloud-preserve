3c5ea645796d058870a059202383b8342ce90493
Mark Cooper <551470+mark-cooper@users.noreply.github.com>
2026-05-07 17:12:58 -0700

    Update legacy duracloud key

--- terraform/modules/stack/_locals.tf
+++ terraform/modules/stack/_locals.tf
@@ -48,7 +48,7 @@ locals {
   expire_aborted_multipart_days           = 3
   expire_legacy_duracloud_file_days       = 3
   expire_noncurrent_version_days          = 14
-  lifecycle_legacy_duracloud_file_tag_key = "ExpireDuraCloudFile"
+  lifecycle_legacy_duracloud_file_tag_key = "ObsoleteDuraCloudFile"
   lifecycle_legacy_duracloud_file_tag_val = "true"
   storage_transition_days                 = 3
 
